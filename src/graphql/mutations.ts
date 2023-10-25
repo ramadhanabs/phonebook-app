8ba6348446a9f5fbb0a7aa652a03b3b4f15fc896
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-10-25 23:52:38 +0700

    feat: migration from apollo to urql

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client"
+import { gql } from "urql"
 
 export const POST_CONTACT = gql`
   mutation AddContactWithPhones(
@@ -29,6 +29,22 @@ export const UPDATE_CONTACT = gql`
       last_name
       phones {
         number
+      }
+    }
+  }
+`
+
+export const UPDATE_CONTACT_PHONE_NUMBER = gql`
+  mutation EditPhoneNumber($pk_columns: phone_pk_columns_input!, $new_phone_number: String!) {
+    update_phone_by_pk(pk_columns: $pk_columns, _set: { number: $new_phone_number }) {
+      contact {
+        id
+        last_name
+        first_name
+        created_at
+        phones {
+          number
+        }
       }
     }
   }
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client"
+import { gql } from "urql"
 
 export const GET_CONTACT_LIST = gql`
   query GetContactList(
