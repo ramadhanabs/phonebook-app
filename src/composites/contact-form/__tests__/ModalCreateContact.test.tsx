774c16966b9effbd33bab8710ed49c1a3c144733
Ramadhana Bagus Solichuddin <ramadhanabagus99@gmail.com>
2023-10-26 10:33:56 +0700

    test: change test message

--- src/composites/contact-form/__tests__/ModalCreateContact.test.tsx
+++ src/composites/contact-form/__tests__/ModalCreateContact.test.tsx
@@ -1,5 +1,5 @@
 import TestProvider from "@/tests/TestProvider"
 
 describe("Modal Create Contact", () => {
-  it("If user click button create contact, modal contact form should be rendered", () => {})
+  it("If user click button create contact, modal contact form should be render", () => {})
 })
